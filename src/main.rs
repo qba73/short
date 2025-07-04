6abeabc0850f5ca6b81705b5f44167ff47ea6995
Jakub Jarosz <jakub@jarosz.dev>
2025-07-04 07:04:25 +0100

    initial sketch
    Signed-off-by: Jakub Jarosz <jakub@jarosz.dev>

---
+++ src/main.rs
@@ -0,0 +1,78 @@
+use rand::{Rng, distr::Alphanumeric};
+use std::{
+    env,
+    fs::{File, OpenOptions},
+    io::{BufRead, BufReader, Write},
+};
+
+fn main() {
+    let mapping_path = "src/mapping.txt";
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() < 2 {
+        println!("Usage: shorten <url>");
+        return;
+    }
+
+    let url = &args[1];
+
+    // valid url
+    if url.starts_with("http") {
+        let mut rng = rand::rng();
+        let short_url: String = std::iter::repeat(())
+            .map(|()| rng.sample(Alphanumeric) as char)
+            .take(8)
+            .collect();
+
+        println!("Long URL: {}", url);
+        println!("Short URL: {}", short_url);
+
+        // storage
+        let mut file_store = match OpenOptions::new()
+            .write(true)
+            .append(true)
+            .open(mapping_path)
+        {
+            Ok(file) => file,
+            Err(_) => {
+                println!("error opening file store for URLs");
+                return;
+            }
+        };
+
+        let mapping = format!("{},{}\n", short_url, url);
+        if let Err(_) = file_store.write_all(mapping.as_bytes()) {
+            println!("error writing to file store");
+            return;
+        }
+    } else {
+        let file_store = match File::open(mapping_path) {
+            Ok(file) => file,
+            Err(_) => {
+                println!("error opening file store");
+                return;
+            }
+        };
+        let reader = BufReader::new(file_store);
+        for line in reader.lines() {
+            let mapping = match line {
+                Ok(line) => line,
+                Err(_) => {
+                    println!("error reading file store");
+                    continue;
+                }
+            };
+            let parts: Vec<&str> = mapping.split(',').collect();
+            if parts.len() != 2 {
+                continue;
+            }
+            let short = parts[0];
+            let long = parts[1];
+            if short == url {
+                println!("Redirecting to {}", long);
+                return;
+            }
+        }
+        println!("Short URL not found");
+    }
+}
